c5b2a11507fb9012fc871b1ebd6c0b60fa2bfbe7
Drott1 <Marius@ma-koppang.com>
2026-09-10 21:54:31 +0200

    Add files via upload
    Historik ändrar så den passar pullups

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="min-traning-v13";
+const CACHE="min-traning-v14";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
