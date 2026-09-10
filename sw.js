20f84916478a068642cb0f54bee54ca70c0535ad
Drott1 <Marius@ma-koppang.com>
2026-09-10 22:27:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="min-traning-v15";
+const CACHE="min-traning-v16";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
