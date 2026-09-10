8d480ceffd35d333ddb9601a892984a04672ab06
Drott1 <Marius@ma-koppang.com>
2026-09-10 20:40:51 +0200

    Add files via upload
    Fixade feedback bettende på input fekt

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="min-traning-v10";
+const CACHE="min-traning-v11";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
