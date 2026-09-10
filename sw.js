b3a65bdf7527a0bbe9ed71b6ff11ec3e1d06c012
Drott1 <Marius@ma-koppang.com>
2026-09-10 21:23:40 +0200

    Add files via upload
    Fixade farmers carry

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="min-traning-v12";
+const CACHE="min-traning-v13";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
