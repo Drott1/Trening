d1076cdbc6e19dd531a794cbcb377657411a478e
Drott1 <Marius@ma-koppang.com>
2026-09-10 20:28:52 +0200

    Add files via upload
    Fixat data migration

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="min-traning-v9";
+const CACHE="min-traning-v10";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
