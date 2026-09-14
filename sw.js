d40dd9464f1fb8ed9a2a60ac939426cf96b74ecb
Drott1 <Marius@ma-koppang.com>
2026-09-14 21:30:39 +0200

    Add files via upload
    Hemma variant

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="min-traning-v21";
+const CACHE="min-traning-v22";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
